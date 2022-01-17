57bfb540b24db184f27fcaa9c4d8aeb5f12e50c6
Holger Mueller <9jholger@gmail.com>
2022-01-17 12:16:54 -0600

    somework

--- index.js
+++ index.js
@@ -10,13 +10,54 @@ $(document).ready(() => {
     e.preventDefault();
 
     if (operator === "") {
-      firstNumber += $(this).val();
-      console.log(firstNumber);
+      firstNumber += e.target.value;
+      result = firstNumber;
+    } else {
+      secondNumber += e.target.value;
+      result = secondNumber;
     }
+
+    handleDisplay();
   });
 
   $("#clear").on("click", (e) => {
     e.preventDefault();
     $("#display").text("0");
+    result = "";
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
   });
+
+  $(".operator").on("click", (e) => {
+    operator = e.target.value;
+  });
+
+  handleSymbol = () => {
+    if (operator === "") {
+      operator = e.target.value;
+    } else {
+      handleCalculation();
+    }
+  };
+
+  $("#equals").on("click", () => {
+    console.log("=");
+    if (operator === "+") {
+      result = parseInt(firstNumber, 10) + parseInt(secondNumber, 10);
+    } else if (operator === "-") {
+      result = parseInt(firstNumber, 10) - parseInt(secondNumber, 10);
+    } else if (operator === "x") {
+      result = parseInt(firstNumber, 10) * parseInt(secondNumber, 10);
+    } else if (operator === "/") {
+      result = parseInt(firstNumber, 10) / parseInt(secondNumber, 10);
+    }
+    result = result;
+
+    handleDisplay();
+  });
+
+  handleDisplay = () => {
+    $("#display").text(result);
+  };
 });
